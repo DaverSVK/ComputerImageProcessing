d099347c35669d96883ab9dddc60058dc27263a3
David <david.szepvolgyi@gmail.com>
2024-02-29 19:31:47 +0100

    camera circle det with adjustable Par1 Par2

--- main.py
+++ main.py
@@ -1,55 +1,97 @@
+# import numpy as np
+# import cv2 as cv
+# import glob
+# # termination criteria
+# criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+# objp = np.zeros((7*5,3), np.float32)
+# objp[:,:2] = np.mgrid[0:7,0:5].T.reshape(-1,2)
+# # Arrays to store object points and image points from all the images.
+# objpoints = [] # 3d point in real world space
+# imgpoints = [] # 2d points in image plane.
+# images = glob.glob('Recources/*.jpg')
+# counter = 1
+# for fname in images:
+#     img = cv.imread(fname)
+#     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#     # Find the chess board corners
+#     ret, corners = cv.findChessboardCorners(gray, (7,5), None)
+#     # If found, add object points, image points (after refining them)
+#     counter+=1
+#     if ret == True:
+#         objpoints.append(objp)
+#         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+#         imgpoints.append(corners2)
+#         # Draw and display the corners
+#
+#         cv.drawChessboardCorners(img, (7,5), corners2, ret)
+#         cv.imshow('img'+str(counter), img)
+#         cv.waitKey(1000)
+# cv.destroyAllWindows()
+#
+# ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+# print("Done")
+# img = cv.imread('Recources/test8.jpg')
+# h,  w = img.shape[:2]
+# newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+# dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+# # crop the image
+# x, y, w, h = roi
+# dst = dst[y:y+h, x:x+w]
+# cv.imwrite('calibresult.png', dst)
+#
+# # undistort
+# mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+# dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+# # crop the image
+# x, y, w, h = roi
+# dst = dst[y:y+h, x:x+w]
+# cv.imwrite('calibresult2.png', dst)
+#
+# mean_error = 0
+# for i in range(len(objpoints)):
+#     imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+#     error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+#     mean_error += error
+# print( "total error: {}".format(mean_error/len(objpoints)) )
+
 import numpy as np
-import cv2 as cv
-import glob
-# termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((7*5,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:5].T.reshape(-1,2)
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-images = glob.glob('Recources/*.jpg')
-counter = 1
-for fname in images:
-    img = cv.imread(fname)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,5), None)
-    # If found, add object points, image points (after refining them)
-    counter+=1
-    if ret == True:
-        objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners2)
-        # Draw and display the corners
+import cv2
 
-        cv.drawChessboardCorners(img, (7,5), corners2, ret)
-        cv.imshow('img'+str(counter), img)
-        cv.waitKey(1000)
-cv.destroyAllWindows()
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-print("Done")
-img = cv.imread('Recources/test8.jpg')
-h,  w = img.shape[:2]
-newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-dst = cv.undistort(img, mtx, dist, None, newcameramtx)
-# crop the image
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
-cv.imwrite('calibresult.png', dst)
+def nothing(x):
+    pass
+# Create a black image, a window
+img = np.zeros((300,512,3), np.uint8)
+cv2.namedWindow('image')
+# create trackbars for color change
+cv2.createTrackbar('Param1','image',10,300,nothing)
+cv2.createTrackbar('Param2','image',10,300,nothing)
 
-# undistort
-mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
-dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
-# crop the image
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
-cv.imwrite('calibresult2.png', dst)
 
-mean_error = 0
-for i in range(len(objpoints)):
-    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
-    mean_error += error
-print( "total error: {}".format(mean_error/len(objpoints)) )
+cap = cv2.VideoCapture(1)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read(0)
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    param1Val = cv2.getTrackbarPos('Param1','image')
+    param2Val = cv2.getTrackbarPos('Param2','image')
+    circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=param1Val,param2=param2Val,minRadius=0,maxRadius=0)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            # draw the outer circle
+            cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
+    cv2.imshow('image',frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+cv2.destroyAllWindows()
