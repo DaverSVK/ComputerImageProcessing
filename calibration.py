50d9a5de0000afb56a1ba31b2c2282f7150e83a4
David <david.szepvolgyi@gmail.com>
2024-03-07 17:11:52 +0100

    Swapable Main

---
+++ calibration.py
@@ -0,0 +1,119 @@
+#_---------------------------------------------------
+# __name__          "Camera Calibration"
+# __author__        "Dávid Szépvölgyi && Jakub Rabčan"
+# __version__       "1.0.0"
+# __maintainer__    "Dávid Szépvölgyi && Jakub Rabčan"
+# __email__         "david.szepvolgyi@gmail.com"
+# __status__        "Production"
+#-----------------------------------------------------
+
+from ximea import xiapi
+import cv2
+import numpy as np
+import glob
+
+
+#Function takes nOfPicks and will store them in "Recources/" as input it needs int number
+def makeCameraPictures(nOfPics):
+    ### runn this command first echo 0|sudo tee /sys/module/usbcore/parameters/usbfs_memory_mb  ###
+
+    #create instance for first connected camera
+    cam = xiapi.Camera()
+
+
+    #start communication
+    #to open specific device, use:
+    #cam.open_device_by_SN('41305651')
+    #(open by serial number)
+    print('Opening first camera...')
+    cam.open_device()
+
+    #settings
+    cam.set_exposure(10000)
+    cam.set_param('imgdataformat','XI_RGB32')
+    cam.set_param('auto_wb', 1)
+    print('Exposure was set to %i us' %cam.get_exposure())
+
+    #create instance of Image to store image data and metadata
+    img = xiapi.Image()
+
+    #start data acquisition
+    print('Starting data acquisition...')
+    cam.start_acquisition()
+
+    j=1
+    while cv2.waitKey() != ord('q'):
+        cam.get_image(img)
+        image = img.get_image_data_numpy()
+        image = cv2.resize(image,(240,240))
+        cv2.imshow("test", image)
+        cv2.waitKey(1)
+        k = cv2.waitKey(0)
+        if k == ord(" "):
+            cv2.imwrite("Recources/test" + str(j) + ".jpeg", image)
+        j = j + 1
+        if j==nOfPics:
+            break
+
+    #stop data acquisition
+    print('Stopping acquisition...')
+    cam.stop_acquisition()
+
+    #stop communication
+    cam.close_device()
+
+    print('Done.')
+
+def calibrateTheCameraUsingUdistortion():
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((5*7,3), np.float32)
+    objp[:,:2] = np.mgrid[0:7,0:5].T.reshape(-1,2)
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    images = glob.glob('*.jpeg')
+    u = 0
+    for fname in images:
+        u +=1
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (7,5), None)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+            corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+            imgpoints.append(corners2)
+            # Draw and display the corners
+            cv2.drawChessboardCorners(img, (7,5), corners2, ret)
+            cv2.imshow('img'+ str(u) , img)
+            cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    img = cv2.imread('test5 .jpeg')
+    h,  w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+    cv2.imwrite('calibresult.png', dst)
+
+    # undistort
+    mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+    dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+    cv2.imwrite('calibresult2.png', dst)
+
+    mean_error = 0
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+        mean_error += error
+    print( "total error: {}".format(mean_error/len(objpoints)) )
+
---
+++ houghCirclesFinder.py
@@ -0,0 +1,91 @@
+# _---------------------------------------------------
+# __name__          "Hough Circles Finder"
+# __author__        "Dávid Szépvölgyi && Jakub Rabčan"
+# __version__       "1.0.0"
+# __maintainer__    "Dávid Szépvölgyi && Jakub Rabčan"
+# __email__         "david.szepvolgyi@gmail.com"
+# __status__        "Production"
+# -----------------------------------------------------
+
+from ximea import xiapi
+import cv2
+import numpy as np
+
+# Function takes input from camera and do algoritm whitch finds circles on current frame/image
+def startVideoFinding():
+    ### runn this command first echo 0|sudo tee /sys/module/usbcore/parameters/usbfs_memory_mb  ###
+
+    # create instance for first connected camera
+    cam = xiapi.Camera()
+
+    # start communication
+    # to open specific device, use:
+    # cam.open_device_by_SN('41305651')
+    # (open by serial number)
+    print('Opening first camera...')
+    cam.open_device()
+
+    # settings
+    cam.set_exposure(10000)
+    cam.set_param('imgdataformat', 'XI_RGB32')
+    cam.set_param('auto_wb', 1)
+    print('Exposure was set to %i us' % cam.get_exposure())
+
+    # create instance of Image to store image data and metadata
+    img = xiapi.Image()
+
+    # start data acquisition
+    print('Starting data acquisition...')
+    cam.start_acquisition()
+
+    def nothing(x):
+        pass
+
+    # Create a black image, a window
+    cv2.namedWindow('detected circles')
+    # create trackbars for color change
+    cv2.createTrackbar('param1', 'detected circles', 10, 300, nothing)
+    cv2.createTrackbar('param2', 'detected circles', 10, 300, nothing)
+    cv2.createTrackbar('dp', 'detected circles', 1, 300, nothing)
+    cv2.createTrackbar('mindist', 'detected circles', 1, 300, nothing)
+    cv2.createTrackbar('minRad', 'detected circles', 0, 300, nothing)
+    cv2.createTrackbar('maxRad', 'detected circles', 0, 300, nothing)
+    # create switch for ON/OFF functionality
+
+    j = 1
+    while cv2.waitKey(1) != ord('q'):
+        cam.get_image(img)
+        image = img.get_image_data_numpy()
+        image = cv2.resize(image, (240, 240))
+        cimg = image
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        param1Val = cv2.getTrackbarPos('param1', 'detected circles')
+        param2Val = cv2.getTrackbarPos('param2', 'detected circles')
+        dpVal = cv2.getTrackbarPos('dp', 'detected circles')
+        mindisVal = cv2.getTrackbarPos('mindist', 'detected circles')
+        minRadVal = cv2.getTrackbarPos('minRad', 'detected circles')
+        maxRadVal = cv2.getTrackbarPos('maxRad', 'detected circles')
+        circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, dpVal, mindisVal, param1=param1Val, param2=param2Val,
+                                   minRadius=minRadVal, maxRadius=maxRadVal)
+
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, 200]:
+                # draw the outer circle
+                cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                # draw the center of the circle
+                cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+        cv2.imshow('detected circles', cimg)
+
+        cv2.waitKey(1)
+
+    # stop data acquisition
+    print('Stopping acquisition...')
+    cam.stop_acquisition()
+
+    # stop communication
+    cam.close_device()
+
+    print('Done.')
+    cv2.destroyAllWindows()
